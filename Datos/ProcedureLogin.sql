3bc0feba680315f2abe5403c1aa502fbabb531ae
maxi758 <maxi758@gmail.com>
2023-12-01 18:49:03 -0300

    agregado login

---
+++ Datos/ProcedureLogin.sql
@@ -0,0 +1,25 @@
+﻿/* se cambia el delimitador de linea para poder almacenar en
+el sistema gestor el código del procedimiento
+Se puede utilizar cualquier caracater 
+*************************************************   */
+
+delimiter //  
+create procedure IngresoLogin(in Usu varchar(20),in Pass varchar(15))
+
+/* =============================================================================
+Se colocan dos parametros de entrada por eso son in
+uno para el nombre de usuario y el otro para la contraseña
+observar que la longitud debe ser igual que la longitud del atributo de la tabla
+===================================================================================  */
+begin
+  /* proyecto en la consulta el rol que tiene el usuario que ingresa */
+  
+  select NomRol
+	from usuario u inner join roles r on u.RolUsu = r.RolUsu
+		where NombreUsu = Usu and PassUsu = Pass /* se compara con los parametros */
+			and Activo = 1; /* el usuario debe estar activo */
+
+
+end 
+//
+
